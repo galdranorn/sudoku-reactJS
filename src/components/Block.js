a028fa4899e032f4a5eebd8f5f8b93806df6e648
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-08 16:34:07 +0100

    Add loops for rows and columns classes

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Tile from './Tile';
 
-// create object with blocks classes (1-9) and tiles IDs (1-81)
-let tileData = [];
+// define objects for tiles IDs
+let blocks = [];
+let columns = {};
+let rows = {};
 
+// blocks classes + tiles IDs
 for (let i=1; i<=9; i++) {
     let ind = [0, 1, 4, 7, 28, 31, 34, 55, 58, 61]
-    let blockClass = {
+    let blockObj = {
         class: 'block'+i,
         idList: {
             row1: [ind[i], ind[i]+1, ind[i]+2], 
@@ -14,23 +17,43 @@ for (let i=1; i<=9; i++) {
             row3: [ind[i]+18, ind[i]+19, ind[i]+20]
         }
     }
-    tileData.push(blockClass);
+    blocks.push(blockObj);
 }
+console.log(blocks);
+
+// column classes + tiles IDs
+for (let i=1; i<10; i++) {
+  let column = [i, i+9, i+18, i+27, i+36, i+45, i+54, i+63, i+72];
+  let columnName = 'column'+i;
+  columns[columnName] = column
+}
+console.log(columns);
+
+// row classes + tiles IDs
+var index = 1;
+for (let i=1; i<=73; i+=9) {
+  let row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8];
+  let rowName = 'row'+index;
+  rows[rowName] = row;
+  index++;
+}
+console.log(rows);
+
 
 const Block = (props) => {
 
-    let row1 = tileData[props.block].idList.row1.map(function(element) {
+    let row1 = blocks[props.block].idList.row1.map(function(element) {
         return (<Tile id={element}/>)
     })
-    let row2 = tileData[props.block].idList.row2.map(function(element) {
+    let row2 = blocks[props.block].idList.row2.map(function(element) {
         return (<Tile id={element}/>)
     })
-    let row3 = tileData[props.block].idList.row3.map(function(element) {
+    let row3 = blocks[props.block].idList.row3.map(function(element) {
         return (<Tile id={element}/>)
     })
 
     return (
-        <form className={tileData[props.block].class}>
+        <form className={blocks[props.block].class}>
             <div className="row">{row1}</div>
             <div className="row">{row2}</div>
             <div className="row">{row3}</div>
