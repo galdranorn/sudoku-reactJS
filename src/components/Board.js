65f34aaf43242c6e5e747b472c1f5fe10d96bcfd
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-08 19:30:16 +0100

    Start from the beginning with sudoku-umd library

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import style from './Board.css';
-import Block from './Block';
+import sudoku from 'sudoku-umd';
+
+// indexes for chunks
+let chunkIndexes = {};
+for (let i=0; i<9; i++) {
+    let ind = [0, 1, 4, 7, 28, 31, 34, 55, 58, 61]
+    let chunkName = 'chunk'+i;
+    let chunk = [ind[i], ind[i]+1, ind[i]+2, ind[i]+9, ind[i]+10, ind[i]+11, ind[i]+18, ind[i]+19, ind[i]+20]; 
+    chunkIndexes[chunkName] = chunk;
+}
+// indexes for columns
+let columnsIndexes = {};
+for (let i=0; i<9; i++) {
+    let column = [i, i+9, i+18, i+27, i+36, i+45, i+54, i+63, i+72];
+    let columnName = 'column'+i;
+    columnsIndexes[columnName] = column;
+  }
+// indexes for rows
+let rowsIndexes = {};
+var index = 1;
+for (let i=0; i<73; i+=9) {
+  let row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8];
+  let rowName = 'row'+index;
+  rowsIndexes[rowName] = row;
+  index++;
+}
 
 const Board = () => {
     return (
         <div className={style.Board}>
-            <div className="blockRow">
-                <Block block="0"/>
-                <Block block="1"/>
-                <Block block="2"/>
-            </div>
-            <div className="blockRow">
-                <Block block="3"/>
-                <Block block="4"/>
-                <Block block="5"/>
-            </div>
-            <div className="blockRow">
-                <Block block="6"/>
-                <Block block="7"/>
-                <Block block="8"/>
-            </div>
         </div>
     )
 }
